030018833a95dc42cf5510514dd5496c868b4529
anshhoff <anshhoff@github.com>
2025-08-16 09:40:04 +0530

    Initial Commit

---
+++ MediPortal/MediPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediPortalApp.swift
+//  MediPortal
+//
+//  Created by Ansh Hardaha on 16/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MediPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
